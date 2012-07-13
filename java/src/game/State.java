4895bddbbb9c875414f27ac9a90dd3908d9a196a
Yi Dai <plmday@gmail.com>
2012-07-13 22:59:53 +0200

    Minor refactoring.

--- java/src/game/State.java
+++ java/src/game/State.java
@@ -40,7 +40,7 @@ public class State {
     
     int rcol = -1;
     int rrow = -1;
-    int lambas = 0;
+    int lambdas = 0;
     
     for (int col = 0; col < board.width; ++col)
       for (int row = 0; row < board.height; ++row)
@@ -50,7 +50,7 @@ public class State {
           rrow = row;
           break;
         case Lambda:
-          lambas++;
+          ++lambdas;
           break;
         default:
           ;
@@ -58,7 +58,7 @@ public class State {
     
     this.robotCol = rcol;
     this.robotRow = rrow;
-    this.lambdasLeft = lambas;
+    this.lambdasLeft = lambdas;
   }
   
   public State makeFinal() {
