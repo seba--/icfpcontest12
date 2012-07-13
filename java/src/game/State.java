6d47027e42a8227daac0d3a6e02c31c7562086d1
Sebastian Erdweg <seba@informatik.uni-marburg.de>
2012-07-13 18:22:54 +0200

    store robot position

--- java/src/game/State.java
+++ java/src/game/State.java
@@ -7,8 +7,22 @@ public class State {
   public Board board;
   public int score;
   
+  public int robotCol;
+  public int robotRow;
+  
   public State(Board board) {
     this.board = board;
     this.score = 0;
+    findRobot();
+  }
+  
+  public void findRobot() {
+    for (int col = 0; col < board.width; col++)
+      for (int row = 0; row < board.height; row++)
+        if (board.grid[col][row] == Cell.Robot) {
+          robotCol = col;
+          robotRow = row;
+          return;
+        }
   }
 }
