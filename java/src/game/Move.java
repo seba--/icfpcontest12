5296d1991e0550085b68084456fcb2e1a2acba75
Sebastian Erdweg <seba@informatik.uni-marburg.de>
2012-07-13 16:19:19 +0200

    enum for moves

---
+++ java/src/game/Move.java
@@ -0,0 +1,51 @@
+package game;
+
+/**
+ * @author seba
+ * 
+ * Moves as specified in Section 2.2
+ */
+public enum Move {
+  L, R, U, D, W, A;
+  
+  public String longName() {
+    switch (this) {
+    case L:
+      return "Left";
+    case R:
+      return "Right";
+    case U:
+      return "Up";
+    case D:
+      return "Down";
+    case W:
+      return "Wait";
+    case A:
+      return "Abort";
+    default:
+      throw new IllegalStateException("Unknown move " + this);
+    }
+  }
+  
+  public static Move parse(String s) {
+    if (s == null || s.isEmpty())
+      return null;
+    
+    switch (s.charAt(0)) {
+    case 'L':
+      return L;
+    case 'R':
+      return R;
+    case 'U':
+      return U;
+    case 'D':
+      return D;
+    case 'W':
+      return W;
+    case 'A':
+      return A;
+    default:
+      throw new IllegalArgumentException("Cannot parse move " + s);
+    }
+  }
+}
