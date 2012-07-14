043dfc203767f2dde177a1db4333312667d3c464
Thomas Horstmeyer <horstmey@informatik.uni-marburg.de>
2012-07-14 23:09:24 +0200

    TH: new single step strategies

---
+++ java/src/game/strategy/DownStrategy.java
@@ -0,0 +1,28 @@
+package game.strategy;
+
+import java.util.Collections;
+import java.util.List;
+
+import game.Command;
+import game.State;
+import game.ai.Strategy;
+
+public class DownStrategy extends Strategy {
+  private  final static List<Command> cmds = Collections.singletonList(Command.Down); 
+  
+  @Override
+  public List<Command> apply(State state) {
+    return cmds;
+  }
+  
+  @Override
+  public boolean isUseOnce() {
+    return true;
+  }
+  
+  @Override
+  public boolean wantsToApply(State s) {
+    int pos = (s.robotCol - 1)* s.board.height + s.robotRow;
+    return s.board.isEarth(pos) || s.board.isEmpty(pos) || s.board.isLambda(pos);
+  }
+}
