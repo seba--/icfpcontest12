beab14bf4d83da7e00b2b2cd79c10eda422919b9
Sebastian Erdweg <seba@informatik.uni-marburg.de>
2012-07-14 10:52:09 +0200

    fixed "interrupted" error message

--- java/src/interrupt/ExitHandler.java
+++ java/src/interrupt/ExitHandler.java
@@ -19,7 +19,7 @@ public class ExitHandler extends Thread {
   }
   
   public void run() {
-    System.err.println("iterrupted");
+    System.out.println("interrupted");
     //TODO: print out result
     driver.printSolution();
   }
