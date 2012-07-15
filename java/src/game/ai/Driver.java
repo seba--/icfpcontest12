701f7b56fcf0c0045b4f3cad5fb79f8a05d2c235
Tillmann Rendel <rendel@informatik.uni-marburg.de>
2012-07-15 03:06:12 +0200

    Cleanup driver a bit and add timing data to log output.

--- java/src/game/ai/Driver.java
+++ java/src/game/ai/Driver.java
@@ -32,42 +32,29 @@ import util.Pair;
  * @author Tillmann Rendel
  * @author Thomas Horstmeyer
  */
-
-/**
- * @author User
- *
- */
-/**
- * @author User
- *
- */
-/**
- * @author User
- *
- */
 public class Driver {
-
   public static final int PRIORITY_QUEUE_CAPACITY = 5000;
 
-  // TODO is contains on PriorityQueue fast enough?
-  
   public final StaticConfig sconfig;
   public final State initialState;
   public final MultiStepper stepper;
   public Comparator<State> comparator = new FitnessComparator();
   public PriorityQueue<State> liveStates = new PriorityQueue<State>(PRIORITY_QUEUE_CAPACITY, comparator);
-  //public Set<State> seenStates = new HashSet<State>();
+  // public Set<State> seenStates = new HashSet<State>();
   public HashMap<Integer, State> seenStates = new HashMap<Integer, State>();
   public Fitness fitness;
   public Selector strategySelector;
 
   public State bestState;
-  
+
   public boolean finished = false;
   public int iterations;
   public boolean timed = false;
   public long endTime = 0;
   public boolean simulate;
+
+  public long lastPrintInfoTime;
+  public int lastPrintInfoIter;
 
   public Driver(StaticConfig sconfig, State initialState, Selector strategySelector, Fitness fitness, boolean simulate) {
     this.sconfig = sconfig;
@@ -77,15 +64,15 @@ public class Driver {
     this.stepper = new MultiStepper(sconfig);
     this.simulate = simulate;
   }
-  
-  
+
   /**
- * @param sconfig
- * @param initialState
- * @param strategySelector
- * @param fitness
- * @param lifetime max runtime in seconds
- */
+   * @param sconfig
+   * @param initialState
+   * @param strategySelector
+   * @param fitness
+   * @param lifetime
+   *          max runtime in seconds
+   */
   public Driver(StaticConfig sconfig, State initialState, Selector strategySelector, Fitness fitness, boolean simulate, int lifetime) {
     this.sconfig = sconfig;
     this.initialState = initialState;
@@ -96,7 +83,7 @@ public class Driver {
     this.timed = true;
     this.simulate = simulate;
   }
-  
+
   public void solve(State initial) {
     strategySelector.prepareState(initial);
     liveStates.add(initial);
@@ -107,36 +94,25 @@ public class Driver {
     // TODO when to stop?
 
     iterations = 0;
-    
+
     // choose k, M, G more cleverly
     // choose 5000k more cleverly
     // explain final solution (what strategies)
 
-    Log.printf(" iter  |  score  |  live   |  dead   \n");
-    Log.printf(" ------+---------+---------+---------\n");
-
+    printDataHeader();
     while (!liveStates.isEmpty()) {
-      
-      if(timed && System.currentTimeMillis() >= endTime) {
-    	Log.printf("%4dk  |  %5d  |  %4dk  |  %4dk  \n",
-    	  iterations / 1000,
-    	  bestState.score,
-    	  liveStates.size() / 1000,
-    	  (seenStates.size() - liveStates.size()) / 1000);
-    	
-    	break;
+      // TODO why is this needed?
+      if (timed && System.currentTimeMillis() >= endTime) {
+        printDataRow();
+        break;
       }
-    	
+
       iterations++;
-      
+
       State state = liveStates.peek();
-      
+
       if (iterations % 5000 == 0) {
-        Log.printf("%4dk  |  %5d  |  %4dk  |  %4dk  \n",
-            iterations / 1000,
-            bestState.score,
-            liveStates.size() / 1000,
-            (seenStates.size() - liveStates.size()) / 1000);
+        printDataRow();
       }
 
       Strategy strategy = strategySelector.selectStrategy(state);
@@ -153,15 +129,15 @@ public class Driver {
           State newState = computeNextState(state, commands, strategy);
           if (stateShouldBeConsidered(newState)) {
             seenStates.put(newState.board.hashCode(), newState);
-//            Log.printf("%s => %8d old fitness, %8d new fitness, %12d\n", commands.get(0), state.fitness, fitness.evaluate(newState), state.hashCode());
+            // Log.printf("%s => %8d old fitness, %8d new fitness, %12d\n",
+            // commands.get(0), state.fitness, fitness.evaluate(newState),
+            // state.hashCode());
 
             if (newState.score > bestState.score) {
               bestState = newState;
             }
 
-            if (newState.ending == Ending.None &&
-                newState.steps < newState.board.width * newState.board.height &&
-                Scoring.maximalReachableScore(newState) > bestState.score ) {
+            if (newState.ending == Ending.None && newState.steps < newState.board.width * newState.board.height && Scoring.maximalReachableScore(newState) > bestState.score) {
               liveStates.add(newState);
               newState.fitness = fitness.evaluate(newState);
               strategySelector.prepareState(newState);
@@ -170,6 +146,29 @@ public class Driver {
         }
       }
     }
+  }
+
+  private void printDataHeader() {
+    Log.printf(" iter  |  score  |  live   |  dead   |  iter/sec  \n");
+    Log.printf(" ------+---------+---------+---------+------------\n");
+
+    lastPrintInfoIter = iterations;
+    lastPrintInfoTime = System.currentTimeMillis();
+  }
+
+  private void printDataRow() {
+    long now = System.currentTimeMillis();
+    int iterPerSec = 1000 * (iterations - lastPrintInfoIter) / (int) (now - lastPrintInfoTime);
+
+    Log.printf("%4dk  |  %5d  |  %4dk  |  %4dk  |  %7d  \n",
+        iterations / 1000,
+        bestState.score,
+        liveStates.size() / 1000,
+        (seenStates.size() - liveStates.size()) / 1000,
+        iterPerSec);
+
+    lastPrintInfoIter = iterations;
+    lastPrintInfoTime = now;
 
   }
 
@@ -197,13 +196,13 @@ public class Driver {
     System.out.println("A");
     System.out.flush();
   }
-  
+
   public void simulateSolution() {
     if (bestState.solution != null) {
       SingleStepper stepper = new SingleStepper(sconfig);
       SimulateWindow win = null;
-      if(this.simulate) {
-    	win = new SimulateWindow(fitness, stepper);
+      if (this.simulate) {
+        win = new SimulateWindow(fitness, stepper);
       }
       State st = initialState;
       st.fitness = fitness.evaluate(st);
@@ -211,19 +210,19 @@ public class Driver {
 
       Log.println(st);
       Log.println();
-      if(this.simulate) {
-    	win.addState(st);
+      if (this.simulate) {
+        win.addState(st);
       }
       for (Command command : commands) {
         st = stepper.step(st, command);
         st.fitness = fitness.evaluate(st);
-        if(this.simulate) {
+        if (this.simulate) {
           win.addState(st);
         }
-//        Log.println(st);
+        // Log.println(st);
       }
-      if(this.simulate) {
-    	win.setVisible(true);
+      if (this.simulate) {
+        win.setVisible(true);
       }
       Log.println(st);
       Log.println();
@@ -240,13 +239,13 @@ public class Driver {
     result.solution = new Solution(state.solution, commands.toArray(new Command[commands.size()]), strategy);
     return result;
   }
-  
+
   public void finished() {
     this.finished = true;
     printSolution();
     simulateSolution();
   }
-  
+
   public static Driver create(IDriverConfig dconfig, StaticConfig sconfig, State state) {
     Selector selector = dconfig.strategySelector(sconfig, state);
     Fitness fitness = dconfig.fitnessFunction(sconfig, state);
@@ -260,34 +259,39 @@ public class Driver {
     boolean simulate = dconfig.simulateWindow();
     return new Driver(sconfig, state, selector, fitness, simulate, lifetime);
   }
- 
+
   public void run() {
     ExitHandler.register(this);
     solve(initialState);
     ExitHandler.unregister(this);
-    
+
     finished();
   }
-  
+
   private boolean stateShouldBeConsidered(State s) {
     State oldState = seenStates.get(s.board.hashCode());
-    
-    if (oldState == null) return true;
-    
-    //anderes board -> behalten
-    if (!s.board.equals(oldState.board)) return true;
-    
-    //TODO: theoretisch koennen noch mehr verworfen werden, wenn wir mehr aufheben... vllt nicht den aufwand wert.
-   
-    //gleiches board, weniger steps -> behalten
-    if (s.steps < oldState.steps) return true;
-    
-    //gleiches board, weniger stepsUnderwater -> behalten
-    if (s.stepsUnderwater < oldState.stepsUnderwater) return true;
-    
+
+    if (oldState == null)
+      return true;
+
+    // anderes board -> behalten
+    if (!s.board.equals(oldState.board))
+      return true;
+
+    // TODO: theoretisch koennen noch mehr verworfen werden, wenn wir mehr
+    // aufheben... vllt nicht den aufwand wert.
+
+    // gleiches board, weniger steps -> behalten
+    if (s.steps < oldState.steps)
+      return true;
+
+    // gleiches board, weniger stepsUnderwater -> behalten
+    if (s.stepsUnderwater < oldState.stepsUnderwater)
+      return true;
+
     return false;
   }
-  
+
   // TODO add exception handling?
   public static void main(String[] args) throws Exception {
     String text;
@@ -305,7 +309,7 @@ public class Driver {
       public Selector strategySelector(StaticConfig sconfig, State initialState) {
         return new SimpleSelector(sconfig);
       }
-      
+
       @Override
       public Fitness fitnessFunction(StaticConfig sconfig, State initialState) {
         return new AverageFitness(new ScoreFitness(), new StepCountFitness(), new ManhattanDirectedFitness(sconfig));
@@ -315,12 +319,9 @@ public class Driver {
       public boolean simulateWindow() {
         return true;
       }
-      
-      
-      
-    };
-    
-    Driver.create(stdConfig, sconfig, state, 15).run();
 
+    };
+
+    Driver.create(stdConfig, sconfig, state, 15).run();
   }
 }
