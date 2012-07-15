0d05b34025cafe8d75d4cb734baffedbca96170d
Sebastian Erdweg <seba@informatik.uni-marburg.de>
2012-07-15 13:25:23 +0200

    try to avoid dying a bit more

---
+++ java/src/game/fitness/AliveFitness.java
@@ -0,0 +1,31 @@
+package game.fitness;
+
+import game.State;
+import game.ai.Fitness;
+
+/**
+ * Dying is bad, aborting is not so good, winning and continuing is good.
+ * 
+ * @author seba
+ */
+public class AliveFitness implements Fitness {
+
+  /**
+   * @see game.ai.Fitness#evaluate(game.State)
+   */
+  @Override
+  public int evaluate(State state) {
+    switch (state.ending) {
+    case LoseRock:
+    case LoseWater:
+      return 0;
+    case Abort:
+      return 500000;
+    case None:
+    case Win:
+    default:
+      return 1000000;
+    }
+  }
+
+}
