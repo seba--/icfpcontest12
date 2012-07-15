49094d4c81e942b1362208d5daf08de12f83f546
waigie <me@waige.de>
2012-07-16 01:30:50 +0200

    fix Shaving

--- java/src/game/stepper/SingleStepper.java
+++ java/src/game/stepper/SingleStepper.java
@@ -49,8 +49,7 @@ public class SingleStepper {
       st.ending = Ending.Abort;
       return true;
     case Shave:
-      st.shaveBeard(st.robotCol, st.robotRow);
-      return true;
+      return st.shaveBeard(st.robotCol, st.robotRow);
     default:
       throw new IllegalArgumentException("Unknown command " + cmd);
     }
